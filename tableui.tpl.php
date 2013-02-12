96f44ba0338c2296ae409a8f329212f698e577ef
Anna Gerber <anna.m.gerber@gmail.com>
2013-02-12 10:29:37 +1000

    support fullscreen user pref in collation tools

--- tableui.tpl.php
+++ tableui.tpl.php
@@ -1,5 +1,19 @@
 <link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all.css">
 <link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all-gray.css">
+<?php 
+$modulePrefix = arg(0);
+$fullscreen = $user->data['fullscreen'];
+?>
+<div id="metadata"
+ <?php if (user_access('edit metadata')): ?>
+  data-editable="true"
+ <?php endif; ?>
+ <?php if ($fullscreen):?>
+ data-fullscreen="<?php print $fullscreen; ?>"
+ <?php endif; ?>
+ data-moduleprefix="<?php print $modulePrefix; ?>"
+ data-modulepath="<?php print drupal_get_path('module', 'collation'); ?>"
+</div>
 <div id="uiplaceholder"></div>
 <script type="text/javascript">
 var leftScrollPos,rightScrollPos;
